72a409c0799e485e2038dc2ae33311c29f3431e0
Winson Lee <a623719265@gmail.com>
2023-06-29 17:00:09 +0800

    updated components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
 import Layout from "@/layouts/Layout";
 import { Section } from "@/styles/GlobalComponents";
 
+import Accomplishments from "@/components/Accomplishments/Accomplishments";
+import BackgroundAnimation from "@/components/BackgroundAnimation/BackgroundAnimation";
+import Hero from "@/components/Hero/Hero";
+import Projects from "@/components/Projects/Projects";
+import Technologies from "@/components/Technologies/Technologies";
+import Timeline from "@/components/Timeline/Timeline";
+
+
+
+
+
+
 const Home = () => {
     return (
         <Layout>
-            <Section grid>
-            <h1>hello world</h1>
-            </Section>
-        </Layout>
+        <Section grid>
+          <Hero />
+          <BackgroundAnimation />
+        </Section>
+        <Projects />
+        <Technologies />
+        <Timeline />
+        <Accomplishments />
+      </Layout>
       
     )
 }
