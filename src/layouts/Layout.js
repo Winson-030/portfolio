967e248c5d2086254271229cf1f32dc15ca0802f
Winson Lee <mail@winson.dev>
2023-07-01 11:43:09 +0800

    delete google font in layout

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -3,16 +3,16 @@ import React from "react";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import { Container } from "./LayoutStyles";
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['greek'] })
+
+
 
 const Layout = ({ children }) => {
 
     return (
         <Container>
             <Header />
-            <main className={inter.className}>{children}</main>
+            <main >{children}</main>
             <Footer />
         </Container>
     )
