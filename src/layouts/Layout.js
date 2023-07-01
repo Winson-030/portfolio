979820e0a10623c7e1258e31048dad99a36dbb20
Winson Lee <mail@winson.dev>
2023-07-01 13:22:24 +0800

    added vercel analytics

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import { Container } from "./LayoutStyles";
-
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -12,7 +12,9 @@ const Layout = ({ children }) => {
     return (
         <Container>
             <Header />
-            <main >{children}</main>
+            <main >{children}
+            <Analytics />
+            </main>
             <Footer />
         </Container>
     )
